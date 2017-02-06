9d4b7eafbb14c8cd348e6ee9d5b79d66f10df931
Ali Siddiqui <alinsiddiqui@gmail.com>
2017-02-05 16:17:27 -0800

    Initial commit

---
+++ Custom White Balance/InitialViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Custom White Balance
+//
+//  Created by Ali Siddiqui on 2/5/17.
+//  Copyright © 2017 Ali Siddiqui. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
