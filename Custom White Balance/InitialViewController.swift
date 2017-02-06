9676735a8772fbd01effbc0701e4e99ac2285e66
Ali Siddiqui <alinsiddiqui@gmail.com>
2017-02-05 17:58:49 -0800

    Implement image picker

--- Custom White Balance/InitialViewController.swift
+++ Custom White Balance/InitialViewController.swift
@@ -8,11 +8,46 @@
 
 import UIKit
 
-class InitialViewController: UIViewController {
+class InitialViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
+    var imageSelected: UIImage?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setDelegate()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        customizeNavigationBar()
+    }
+    
+    func setDelegate() {
+        imagePicker.delegate = self
+    }
+    
+    func customizeNavigationBar() {
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0)
+    }
+    
+    @IBAction func chooseImage(_ sender: SelectButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageSelected = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
